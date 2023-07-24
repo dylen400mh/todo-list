dfc5f73a3ff05c1cf7ae816ddea2c4a94bb03ead
dylen400mh <dylenbelanger2004@gmail.com>
2023-07-24 16:58:12 -0400

    Remove unused methods inside Project class

--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,7 @@
 export default class Project {
-
     constructor(title) {
         this.title = title;
         this.todos = [];
         this.selected = false;
-    }
-    
-    addTodo(todo) {
-        this.todos.push(todo);
-    }
-
-    removeTodo(todo) {
-        this.todos.pop(todo);
     }
 }
