4fb5ee4dc8e6e00058d15e88cc7f79c6d31ac26d
dylen400mh <dylenbelanger2004@gmail.com>
2023-07-23 20:55:20 -0400

    Add filters to get today's and this week's todos

--- src/Info.js
+++ src/Info.js
@@ -22,6 +22,42 @@ const Info = (() => {
         return getAllTodos().filter(todo => todo.complete);
     }
 
+    const getTodayTodos = () => {
+        const todos = getAllTodos();
+        const today = new Date();
+
+        const todayTodos = todos.filter(todo => {
+            const todoDate = parseDateString(todo.date);
+
+            return (todoDate.getDate() === today.getDate() &&
+                todoDate.getMonth() === today.getMonth() &&
+                todoDate.getFullYear() === today.getFullYear());
+        });
+
+        return todayTodos;
+    }
+
+    const getWeeklyTodos = () => {
+        const todos = getAllTodos();
+        const today = new Date();
+        const oneWeekToday = new Date();
+        oneWeekToday.setDate(oneWeekToday.getDate() + 7);
+
+        const weeklyTodos = todos.filter(todo => {
+            const todoDate = parseDateString(todo.date);
+
+            return (today <= todoDate && todoDate <= oneWeekToday);
+        })
+
+        return weeklyTodos;
+    }
+
+    // converts date string to date object
+    const parseDateString = (dateString) => {
+        const [year, month, day] = dateString.split("-");
+        return new Date(year, month - 1, day) // Month is 0-indexed in Date
+    }
+
     // return array that contains all projects and filters
     const getAllFilters = () => {
         updateFilters();
@@ -44,6 +80,16 @@ const Info = (() => {
         selected: true,
     },
     {
+        title: "Today",
+        todos: getTodayTodos(),
+        selected: false,
+    },
+    {
+        title: "This Week",
+        todos: getWeeklyTodos(),
+        selected: false,
+    },
+    {
         title: "Important",
         todos: getImportantTodos(),
         selected: false
@@ -59,10 +105,14 @@ const Info = (() => {
         const allFilter = filters.find(filter => filter.title === "All");
         const importantFilter = filters.find(filter => filter.title === "Important");
         const completeFilter = filters.find(filter => filter.title === "Completed");
+        const todayFilter = filters.find(filter => filter.title === "Today");
+        const thisWeekFilter = filters.find(filter => filter.title === "This Week");
 
         allFilter.todos = getAllTodos();
         importantFilter.todos = getImportantTodos();
         completeFilter.todos = getCompleteTodos();
+        todayFilter.todos = getTodayTodos();
+        thisWeekFilter.todos = getWeeklyTodos();
     }
 
     return { projects, filters, getAllFilters };
